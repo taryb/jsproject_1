4f1893a8c48d7aac161c0ef987e9b598fa21584e
taryb <tbounavong@gmail.com>
2023-04-24 19:02:43 -0700

    updated code it dont work no more

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-
 const app = express();
+const routes = require('./routes');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'your_mysql_username',
-  password: 'your_mysql_password',
-  database: 'your_mysql_database'
+// set up middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// use the routes defined in routes.js
+app.use('/', routes);
+
+// start the server
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
-
-app.use(bodyParser.json());
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
-
-
-
